bb922874095c3389e27194cf7dd5fbd36b6614b5
Kollár Ádám <75732162+KollarAdam@users.noreply.github.com>
2023-03-19 19:42:30 +0100

    1. commit test

---
+++ projekt_3/projekt_3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekt_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Enemy nem = new Enemy(100,10,"fiú");
+            nem.Skeleton();
+            nem.Magician();
+            Console.WriteLine(nem.ToString());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ projekt_3/projekt_3/enemy.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Deployment.Internal;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekt_3
+{
+    internal class Enemy
+    {
+        Random rnd = new Random();
+        int hp;
+        int dmg;
+        string name;
+        int crit;
+        public Enemy(int hp, int dmg, string name, int crit)
+        {
+            this.hp = hp;
+            this.dmg = dmg;
+            this.name = name;
+            this.crit = crit;
+        }
+        public void Skeleton()
+        {
+            hp = 50;
+            dmg = 5;
+            name = "Skeleton";
+            crit = 5;
+        }
+        public void Magician()
+        {
+            hp = 30;
+            dmg = 7;
+            name = "Magician";
+            crit= 100;
+        }
+        public void Zombie()
+        {
+            hp = 70;
+            dmg = 3;
+            name = "Zombie";
+            crit = 0;
+        }
+        public void Critical()
+        {
+            if (rnd.Next(1,101)<= crit) {
+                dmg = dmg * 2;
+            }
+        }
+        public override string ToString()
+        {
+            return $"A {name} hp-ja= {hp} :)";
+        }
+    }
+}
