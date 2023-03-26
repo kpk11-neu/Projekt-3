632a85040bbcf28c655e26decd656e0f7f1492fe
Kollár Ádám <kollaradam68@gmail.com>
2023-03-26 20:59:46 +0200

    Hozzáadva nehény ellenség

--- projekt_3/projekt_3/enemy.cs
+++ projekt_3/projekt_3/enemy.cs
@@ -41,8 +41,37 @@ namespace projekt_3
             hp = 70;
             dmg = 3;
             name = "Zombie";
+            crit = 10;
+        }
+        public void Slime()
+        {
+            hp = 10;
+            dmg = 2;
+            name = "Slime";
             crit = 0;
         }
+        public void Mummy()
+        {
+            hp = 40;
+            dmg = 4;
+            name = "Mummy";
+            crit = 20;
+        }
+        public void Mimic()
+        {
+            hp = 140;
+            dmg = 10;
+            name = "Zombie";
+            crit = 15;
+        }
+        public void Bat()
+        {
+            hp = 5;
+            dmg = 1;
+            name = "Bat";
+            crit = 0;
+        }
+
         public void Critical()
         {
             if (rnd.Next(1,101)<= crit) {
